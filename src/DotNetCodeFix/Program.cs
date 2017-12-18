cc62659369ddb00c9067f2a561eb9f23b4e2f977
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-12-18 11:22:01 +0700

    Initial commit

---
+++ src/DotNetCodeFix/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using System.Reflection;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using System.Threading;
+
+namespace DotNetCodeFix
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", "/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild");
+
+
+            var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync("/Users/wk/Source/github/DotNetCodeFix/tests/MyApp/MyApp.csproj");
+            // var project = await workspace.OpenProjectAsync("/Users/wk/Source/project/standard/easy-capture/EasyCapture/EasyCapture.csproj");
+
+            Console.WriteLine($"Documents: {project.Documents.Count()}");
+            Console.WriteLine($"AssemblyName: {project.AssemblyName}");
+
+            var analyzers = ImmutableArray.CreateBuilder<DiagnosticAnalyzer>();
+            Assembly.GetAssembly(typeof(AsyncMethodWithoutAsyncSuffixAnalyzer))
+                .GetTypes()
+                .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
+                .Select(Activator.CreateInstance)
+                .Cast<DiagnosticAnalyzer>()
+                .ToList()
+                .ForEach(analyzers.Add);
+
+            Console.WriteLine($"Found {analyzers.Count()}");
+
+            var compilation = await project.GetCompilationAsync();
+
+            var asm = compilation.AssemblyName;
+            Console.WriteLine($"AssemblyName: {asm}");
+
+            var allTypes = compilation.Assembly.TypeNames;
+            Console.WriteLine($"Types: {string.Join(", ", allTypes)}");
+
+
+            foreach (var analyzer in analyzers)
+            {
+                var diagnosticResults = await compilation.WithAnalyzers(ImmutableArray.Create(analyzer)).GetAllDiagnosticsAsync();
+                //var interestingResults = diagnosticResults.Where(x => x.Severity != DiagnosticSeverity.Hidden).ToArray();
+                var interestingResults = diagnosticResults.ToArray();
+                if (interestingResults.Any())
+                {
+                    Console.WriteLine($"Results for analyzer: {analyzer}");
+                }
+
+                foreach (var diagnostic in interestingResults)
+                {
+                    if (diagnostic.Severity != DiagnosticSeverity.Hidden)
+                    {
+                        Console.WriteLine($"Severity: {diagnostic.Severity}");
+                        Console.WriteLine($"Message: {diagnostic.GetMessage()}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/MyApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Program
+    {
+        static async Task X()
+        {
+            await Task.Run(() => { });
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
