cf69b35a6ce602f39a5d19d18a4cc250e5bc6449
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-12-20 16:03:18 +0700

    Changed msbuild path

--- src/DotNetCodeFix/Program.cs
+++ src/DotNetCodeFix/Program.cs
@@ -16,11 +16,13 @@ namespace DotNetCodeFix
     {
         static async Task Main(string[] args)
         {
-            Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", "/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild");
+            // Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", "/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild");
+            Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", "/usr/local/share/dotnet/sdk/2.1.2/MSBuild.dll");
+
             Environment.SetEnvironmentVariable("COREHOST_TRACE", "1");
 
-            // Environment.SetEnvironmentVariable("MSBuildSDKsPath", "/usr/local/share/dotnet/sdk/2.1.2/Sdks");
-            Environment.SetEnvironmentVariable("MSBuildSDKsPath", "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/msbuild/15.0/bin/Sdks");
+            Environment.SetEnvironmentVariable("MSBuildSDKsPath", "/usr/local/share/dotnet/sdk/2.1.2/Sdks");
+            // Environment.SetEnvironmentVariable("MSBuildSDKsPath", "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/msbuild/15.0/bin/Sdks");
 
 
             var workspace = MSBuildWorkspace.Create();
@@ -50,7 +52,6 @@ namespace DotNetCodeFix
 
             var allTypes = compilation.Assembly.TypeNames;
             Console.WriteLine($"Types: {string.Join(", ", allTypes)}");
-
 
             foreach (var analyzer in analyzers)
             {
