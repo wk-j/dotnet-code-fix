38188d3f365f5d2b3d336379ceaebf6838f87c9d
wk-j <somnuk.wk@gmail.com>
2017-12-22 00:04:14 +0700

    Grep codes from StyleCopAnalyzers

---
+++ CMD.md
@@ -0,0 +1,3 @@
+## Links
+
+- https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/f4e3909012d52368cbd9064b92da6293075861d1/StyleCop.Analyzers/StyleCopTester/Program.cs#L198
--- tests/DotNetCodeFix.Buildalyzer/AsyncMethodWithoutAsyncSuffixAnalyzer.cs
+++ tests/DotNetCodeFix.Buildalyzer/AsyncMethodWithoutAsyncSuffixAnalyzer.cs
@@ -7,87 +7,85 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Threading.Tasks;
 
-namespace DotNetCodeFix {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class AsyncMethodWithoutAsyncSuffixAnalyzer : DiagnosticAnalyzer {
-        private const string DiagnosticId = nameof(AsyncMethodWithoutAsyncSuffixAnalyzer);
-        private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AsyncMethodWithoutAsyncSuffixAnalyzer : DiagnosticAnalyzer {
+    private const string DiagnosticId = nameof(AsyncMethodWithoutAsyncSuffixAnalyzer);
+    private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
 
-        private static readonly string Category = "AsyncMethodWithoutAsyncSuffixAnalyzer-Category";
-        private static readonly string Message = "AsyncMethodWithoutAsyncSuffixAnalyzer-Message";
-        private static readonly string Title = "AsyncMethodWithoutAsyncSuffixAnalyzer-Title";
+    private static readonly string Category = "AsyncMethodWithoutAsyncSuffixAnalyzer-Category";
+    private static readonly string Message = "AsyncMethodWithoutAsyncSuffixAnalyzer-Message";
+    private static readonly string Title = "AsyncMethodWithoutAsyncSuffixAnalyzer-Title";
 
-        //private static readonly string Category = VSDiagnosticsResources.AsyncCategory;
-        //private static readonly string Message = VSDiagnosticsResources.AsyncMethodWithoutAsyncSuffixAnalyzerMessage;
-        //private static readonly string Title = VSDiagnosticsResources.AsyncMethodWithoutAsyncSuffixAnalyzerTitle;
+    //private static readonly string Category = VSDiagnosticsResources.AsyncCategory;
+    //private static readonly string Message = VSDiagnosticsResources.AsyncMethodWithoutAsyncSuffixAnalyzerMessage;
+    //private static readonly string Title = VSDiagnosticsResources.AsyncMethodWithoutAsyncSuffixAnalyzerTitle;
 
-        internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, isEnabledByDefault: true);
+    internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, isEnabledByDefault: true);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-        public override void Initialize(AnalysisContext context) {
-            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.MethodDeclaration);
+    public override void Initialize(AnalysisContext context) {
+        context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.MethodDeclaration);
+    }
+
+    private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context) {
+        var method = context.Node as MethodDeclarationSyntax;
+        if (method == null) {
+            return;
         }
 
-        private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context) {
-            var method = context.Node as MethodDeclarationSyntax;
-            if (method == null) {
-                return;
-            }
-
-            if (method.Modifiers.Any(SyntaxKind.OverrideKeyword)) {
-                return;
-            }
-
-            var returnType = context.SemanticModel.GetTypeInfo(method.ReturnType);
-            if (returnType.Type == null) {
-                return;
-            }
-
-            var declaredSymbol = context.SemanticModel.GetDeclaredSymbol(method);
-            if (declaredSymbol == null) {
-                return;
-            }
-
-            if (IsDefinedInAncestor(declaredSymbol)) {
-                return;
-            }
-
-            if (method.Modifiers.Any(SyntaxKind.AsyncKeyword) ||
-                returnType.Type.MetadataName == typeof(Task).Name ||
-                returnType.Type.MetadataName == typeof(Task<>).Name) {
-                if (!method.Identifier.Text.EndsWith("Async")) {
-                    context.ReportDiagnostic(Diagnostic.Create(Rule, method.Identifier.GetLocation(), method.Identifier.Text));
-                }
-            }
+        if (method.Modifiers.Any(SyntaxKind.OverrideKeyword)) {
+            return;
         }
 
-        private static bool IsDefinedInAncestor(IMethodSymbol methodSymbol) {
-            var type = methodSymbol?.ContainingType;
-            if (type == null) {
-                return false;
+        var returnType = context.SemanticModel.GetTypeInfo(method.ReturnType);
+        if (returnType.Type == null) {
+            return;
+        }
+
+        var declaredSymbol = context.SemanticModel.GetDeclaredSymbol(method);
+        if (declaredSymbol == null) {
+            return;
+        }
+
+        if (IsDefinedInAncestor(declaredSymbol)) {
+            return;
+        }
+
+        if (method.Modifiers.Any(SyntaxKind.AsyncKeyword) ||
+            returnType.Type.MetadataName == typeof(Task).Name ||
+            returnType.Type.MetadataName == typeof(Task<>).Name) {
+            if (!method.Identifier.Text.EndsWith("Async")) {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, method.Identifier.GetLocation(), method.Identifier.Text));
             }
+        }
+    }
 
-            var interfaces = type.AllInterfaces;
-            foreach (var @interface in interfaces) {
-                var interfaceMethods = @interface.GetMembers().Select(type.FindImplementationForInterfaceMember);
-                if (interfaceMethods.Any(method => method.Equals(methodSymbol))) {
-                    return true;
-                }
-            }
-
-            // Start with the first ancestor
-            type = type.BaseType;
-            while (type != null) {
-                var methods = type.GetMembers().OfType<IMethodSymbol>().ToArray();
-                if (methods.Any(method => method.Equals(methodSymbol))) {
-                    return true;
-                }
-
-                type = type.BaseType;
-            }
-
+    private static bool IsDefinedInAncestor(IMethodSymbol methodSymbol) {
+        var type = methodSymbol?.ContainingType;
+        if (type == null) {
             return false;
         }
+
+        var interfaces = type.AllInterfaces;
+        foreach (var @interface in interfaces) {
+            var interfaceMethods = @interface.GetMembers().Select(type.FindImplementationForInterfaceMember);
+            if (interfaceMethods.Any(method => method.Equals(methodSymbol))) {
+                return true;
+            }
+        }
+
+        // Start with the first ancestor
+        type = type.BaseType;
+        while (type != null) {
+            var methods = type.GetMembers().OfType<IMethodSymbol>().ToArray();
+            if (methods.Any(method => method.Equals(methodSymbol))) {
+                return true;
+            }
+
+            type = type.BaseType;
+        }
+
+        return false;
     }
 }
